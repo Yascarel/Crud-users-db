ddfba1346ab3fafb9b42457b4f0ab1692a2c4aa6
Alberto Facenda <Yascarelrd@hotmail.com>
2024-03-02 10:55:43 -0400

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const usersRouter = require('./users.router');
 
 // colocar las rutas aquí
-
+router.use(usersRouter);
 
 module.exports = router;
